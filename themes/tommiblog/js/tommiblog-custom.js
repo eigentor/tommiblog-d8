320faeb3d95c7e3aae8d73eb93b0df5241457ce6
Thomas Moseler <tmoseler@gmx.de>
2014-05-19 23:07:31 +0200

    Styling für Mobile Menu und Header responsive

--- themes/tommiblog/js/tommiblog-custom.js
+++ themes/tommiblog/js/tommiblog-custom.js
@@ -1,8 +1,22 @@
 (function ($) {
   
-  $(document).ready(function(){
-    $('body').click(function{
-      alert('Jo mei');
+  // $(document).ready(function(){
+  //   $('body').click(function{
+  //     alert('Jo mei');
+  //   });
+
+  // Open and close the main mobile menu
+    $('.region-top-wide .switch-mobile-main-menu').click(function(){
+       $('.region-top-wide .main-menu-mobile').slideToggle( "normal", function() {
+
+       });
+       // Add the class 'active-menu' so we know the menu is in an open state.
+       // If the class is already there, remove it while closing the menu.
+       if($('.region-top-wide .main-menu-mobile').hasClass('active-menu')) {
+          $('.region-top-wide .main-menu-mobile').removeClass('active-menu');
+        } else {
+         $('.region-top-wide .main-menu-mobile').addClass('active-menu');
+        } 
     });
 
        
