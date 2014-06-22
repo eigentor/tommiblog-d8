8d8ae3beadec1062074b9f289fd2d839efb72599
Thomas Moseler <tmoseler@gmx.de>
2014-06-22 21:54:54 +0200

    zweiter Block für Mobile Menu Switcher

--- themes/tommiblog/js/tommiblog-custom.js
+++ themes/tommiblog/js/tommiblog-custom.js
@@ -5,19 +5,11 @@
   // Open and close the main mobile menu
     $('#block-mobilemenuswitcher').click(function(){
        $('body').toggleClass('mobile-menu-open');
-       /*$('#block-mainnavigation').slideToggle( "normal", function() {
-       });
-       $('#block-socialmediablockheader').slideToggle( "normal", function() {
-       });
-       $('#block-tommiblog-search').slideToggle( "normal", function() {
-       });
-       // Add the class 'active-menu' so we know the menu is in an open state.
-       // If the class is already there, remove it while closing the menu.
-       if($('.region-top-wide .main-menu-mobile').hasClass('active-menu')) {
-          $('.region-top-wide .main-menu-mobile').removeClass('active-menu');
-        } else {
-         $('.region-top-wide .main-menu-mobile').addClass('active-menu');
-        }*/
+       
+    });
+    $('#block-mobilemenuswitcher-2').click(function(){
+       $('body').toggleClass('mobile-menu-open');
+       
     });
 
        
